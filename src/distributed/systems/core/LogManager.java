2ceb5fce9778dc069501275cbed094656504ed7b
Filipe Meirinhos <filimeirinhos@gmail.com>
2013-04-23 14:47:36 +0200

    New LogManager added

--- src/distributed/systems/core/LogManager.java
+++ src/distributed/systems/core/LogManager.java
@@ -13,19 +13,24 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
 
 public class LogManager {
 
 	private String filename;
+	private int[][] orderedClocks;
+	private String[] orderedLogs;
 
-	public LogManager(String filename){
-		this.filename= filename;
-	}
+	public int[][] getOrderedClocks() { return orderedClocks; }
+	public String[] getOrderedLogs() { return orderedLogs; }
+	public LogManager(String filename){ this.filename= filename; }
+	public String getFilename() { return filename; }
 	
-	public String getFilename() {
-		return filename;
+	public void cleanupStructures() {
+		orderedClocks = null;
+		orderedLogs = null;
 	}
 
 	public synchronized void writeToBinary (Object obj, boolean append){
@@ -77,43 +82,53 @@ public class LogManager {
 	}
 
 	//Gets the Log ordered by clocks
-	/*public HashMap<int[], String> readOrderedLog (){
+	public void readOrderedLog (){
 
-		HashMap<int[], String> unorderedLogMap= ReadFromText();
+		cleanupStructures();
+		HashMap<int[], String> unorderedLogMap= readLogFromTextfile();
 		
 		Set<int[]> clock = unorderedLogMap.keySet();
-		
+
+		/* 
+		 * Option 1
 		VectorialClock[] clocksV = new VectorialClock[clock.size()];
 		int index = 0;
 		for(int[] c : clock) {
 			clocksV[index] = new VectorialClock(c);
 			index++;
 		}
-		
-		ArrayList<String> string = new ArrayList<String>(unorderedLogMap.values());
-		String[] strings = (String[]) string.toArray();
-		
-		VectorialClock tmpClock = null;
+		*/
+
+		/*
+		 * Option 2
+		 */
+		int clocks[][] = (int[][])clock.toArray();
+
+		String[] strings = Arrays.asList(unorderedLogMap.values().toArray()).toArray(new String[unorderedLogMap.values().toArray().length]);
+
 		boolean tradeMade, atLeastOne;
-		if(clocksV.length == 0)
-			return null;
-		int clockLenght = clocksV[0].getClock().length;
-		String aux = null;
+		if(clocks.length == 0)
+			return;
+		int clockLenght = clocks[0].length;
 		
+		//VectorialClock tmpClock = null;
+		int[] tmpClock = null;
+		String aux = null;
+
 		do {
 			tradeMade = false;
-			for (int i = 0; i < clocksV.length - 1; i++) {
+			for (int i = 0; i < clocks.length - 1; i++) {
 				atLeastOne = false;
 				for (int j = 0; j < clockLenght; j++) {
-					if (clocksV[i].getClock()[j] >= clocksV[i + 1].getClock()[j]) {
-						if(clocksV[i].getClock()[j] > clocksV[i + 1].getClock()[j])
+					if (clocks[i][j] >= clocks[i + 1][j]) {
+						if(clocks[i][j] > clocks[i + 1][j])
 							atLeastOne = true;
 						if (j == clockLenght-1 && atLeastOne) {
-							tmpClock = clocksV[i];
+							tmpClock = clocks[i];
 							aux = strings[i];
-							clocksV[i] = clocksV[i + 1];
+							clocks[i] = clocks[i + 1];
 							strings[i]= strings[i+1];
-							clocksV[i + 1] = tmpClock;
+							clocks[i + 1] = tmpClock;
 							strings[i+1] = aux;
 							tradeMade = true;
 						}
@@ -123,36 +138,30 @@ public class LogManager {
 				}
 			}
 		} while (tradeMade);
-		
-		HashMap<int[], String> orderedLog = new HashMap<int[], String>();
-		
-		for(int i = 0 ; i < clocksV.length; i++)
-			orderedLog.put(clocksV[i].getClock(), strings[i]);
 
-		return orderedLog;
-	}*/
+		orderedClocks = clocks;
+		orderedLogs = strings;
+		
+		/*
+		HashMap<int[], String> orderedLog = new HashMap<int[], String>();
+		for(int i = 0 ; i < clocksV.length; i++)
+			//Guardar os clocks algures...
+			*/
+
+	}
 
 	private static class AppendableObjectOutputStream extends ObjectOutputStream {
 		public AppendableObjectOutputStream(OutputStream out) throws IOException {
 			super(out);
 		}
 
-		
+
 		@Override
 		protected void writeStreamHeader() throws IOException {
 			reset();
 		}
 	}
-	
-//
-//
-	//
-	//
-	//
-	///NEW LOG WRITER AS TEXT FORMAT
-	//
-	//
-//
+
 	public synchronized void writeAsText (Object obj, boolean append){
 		try {
 
@@ -160,121 +169,74 @@ public class LogManager {
 
 			FileWriter fw = new FileWriter(file,true);
 			BufferedWriter bw = new BufferedWriter(fw);
-	
-				bw.write(obj.toString());
-				bw.newLine();
-        bw.close();
+
+			bw.write(obj.toString());
+			bw.newLine();
+			bw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//
-	//
-		//
-		//
-		//
-		///NEW LOG READ AS TEXT FORMAT
-		//
-		//
-	//
-		public synchronized HashMap<int[], String> ReadFromText (){
-			int clock[];
-			HashMap<int[], String> log= new HashMap<int[], String>();
-			try {
 
-				File file = new File(filename+".txt");
-				BufferedReader br = new BufferedReader(new FileReader(file));
-				String line;
-				while ((line = br.readLine()) != null) {
-				   // process the line.
-					String[] s1 = line.split("]");
-					String s3 = s1[0].substring(1);
-					String[] s2 = s3.split(",");
-					clock = new int[s2.length];
-					for(int i = 0; i< s2.length; i++){
-						clock[i] = Integer.parseInt(s2[i].trim());
-					}
-					log.put(clock, line);
-					
+	public synchronized HashMap<int[], String> readLogFromTextfile (){
+		int clock[];
+		HashMap<int[], String> log= new HashMap<int[], String>();
+		try {
+			File file = new File(filename+".txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				// process the line.
+				String[] s1 = line.split("]");
+				String s3 = s1[0].substring(1);
+				String[] s2 = s3.split(",");
+				clock = new int[s2.length];
+				for(int i = 0; i< s2.length; i++){
+					clock[i] = Integer.parseInt(s2[i].trim());
 				}
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+				log.put(clock, line);
 			}
-			return log;
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-	
-	
+		return log;
+	}
 
-	/*public void writeLogToTextfile() {
-		// TODO Auto-generated method stub
-		HashMap<int[], String> log = readOrderedLog();
+	/**
+	 * The ordered logs must be ready to use. Check readOrderedLog function.
+	 * @param extensionToFilename
+	 */
+	public void writeOrderedLogToTextfile(String extensionToFilename) {
+		//readOrderedLog();
 		try {
-
-			File file = new File(filename+"_txt");
-
+			File file = new File(filename+extensionToFilename);
 			// if file doesnt exists, then create it
 			if (!file.exists()) {
 				file.createNewFile();
 			}
-
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
-
-			for(String m : log.values()) {
+			for(String m : orderedLogs) {
 				bw.write(m);
 				bw.newLine();
 			}
-
 			bw.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void writeOrderedRestartToTextfile() {
-		// TODO Auto-generated method stub
-		HashMap<int[], String> log = readOrderedLog();
-		try {
 
-			File file = new File(filename+"_txt_beforeRestart");
-
-			// if file doesnt exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-
-			for(String m : log.values()) {
-				bw.write(m);
-				bw.newLine();
-			}
-
-			bw.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-*/
-	/*
 	public LogEntryText[] getLogEntriesOrdered() {
 		String hostname;
 		int port;
 		LogEntryType event; 
 		int[] clock;
 		long jobID = -1;
-	
-		HashMap<int[], String> orderedLog = readOrderedLog();
-		LogEntryText[] entries = new LogEntryText[orderedLog.size()];
+		LogEntryText[] entries = new LogEntryText[orderedLogs.length];
 		int i = 0;
-		for(String s : orderedLog.values()) {
+		for(String s : orderedLogs) {
 			//Parse string, put values in variables
 			String[] s1 = s.split("]");// 0 tem o clock 1 tem o resto
 			String[] s2 = s1[1].split(" "); //Event <JobID> Origin
@@ -290,7 +252,7 @@ public class LogManager {
 				hostname= s3[0];
 				port = Integer.parseInt(s3[1]);
 			}
-			
+
 			s1[0].replace("[","");
 			String[] s4 = s1[0].split(",");
 			clock = new int[s4.length];
@@ -304,5 +266,4 @@ public class LogManager {
 		}
 		return entries;
 	}
-	*/
 }
